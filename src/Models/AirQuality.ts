9c35a0ef79e6477218024eb0171ae6433bec9f4c
Oğuzhan Yamak <oguzhanyamak@outlook.com>
2021-04-16 02:04:08 +0300

    saved

---
+++ src/Models/AirQuality.ts
@@ -0,0 +1,14 @@
+import { JsonAlias, JsonProperty } from "jackson-js"
+
+
+export interface Airquality{
+
+    co:number
+    no2:number
+    o3:number
+    so2:number
+    pm2_5:number
+    pm10:number
+    us_epa_index:number
+    gb_defra_index:number
+}
